f2a891386078e3bc159f241e53a3d696c9cc49f9
Hrishikesh Koundinya <63454195+Hrishikesh015@users.noreply.github.com>
2022-06-23 22:35:56 +0530

    Add files via upload

---
+++ src/frontend/components/App.js
@@ -0,0 +1,74 @@
+
+import logo from './logo.png';
+import './App.css';
+import { ethers } from 'ethers'
+import { useState } from 'react';
+import MarketplaceAbi from '../contractsData/Marketplace.json'
+import MarketplaceAddress from '../contractsData/Marketplace-address.json'
+import NFTAbi from '../contractsData/NFT.json'
+import NFTAddress from '../contractsData/NFT-address.json'
+import Navigation from './Navbar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Spinner } from 'react-bootstrap';
+import Home from './Home.js'
+import Create from './Create.js'
+import MyListedItems from './MyListedItems.js'
+import MyPurchases from './MyPurchases.js'
+function App() {
+  const [loading, setLoading] = useState(true)
+  const [account, setAccount] = useState(null)
+  const [nft, setNFT] = useState({})
+  const [marketplace, setMarketplace] = useState({})
+
+  const web3handler = async () => {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+    setAccount(accounts[0])
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+
+    loadcontracts(signer)
+  }
+
+  const loadcontracts = async (signer) => {
+    const marketplace = new ethers.Contract(MarketplaceAddress.address, MarketplaceAbi.abi, signer)
+    setMarketplace(marketplace)
+    const nft = new ethers.Contract(NFTAddress.address, NFTAbi.abi, signer)
+    setNFT(nft)
+    setLoading(false)
+  }
+  return (
+    <BrowserRouter>
+     <div className="App">
+        <>
+          <Navigation web3handler={web3handler} account={account} />
+        </>
+        <div>
+          {loading ? (
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '80vh' }}>
+              <Spinner animation="border" style={{ display: 'flex' }} />
+              <p className='mx-3 my-0'>Awaiting Metamask Connection...</p>
+            </div>
+          ) : (
+            <Routes>
+              <Route path="/" element={
+                <Home marketplace={marketplace} nft={nft} />
+              } />
+              <Route path="/create" element={
+                <Create marketplace={marketplace} nft={nft} />
+              } />
+              <Route path="/my-listed-items" element={
+                <MyListedItems marketplace={marketplace} nft={nft} account={account} />
+              } />
+              <Route path="/my-purchases" element={
+                <MyPurchases marketplace={marketplace} nft={nft} account={account} />
+              } />
+            </Routes>
+          )}
+        </div>
+      </div>
+  </BrowserRouter>
+    
+  );
+}
+
+export default App;
